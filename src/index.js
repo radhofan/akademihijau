93c9a37663b4c76720b8b400d11d5801c231416d
radhofan <radhofanazizi@gmail.com>
2023-07-07 17:33:05 +0700

    a

--- src/index.js
+++ src/index.js
@@ -110,7 +110,9 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}>
+      <App/>
+    </RouterProvider>
   </React.StrictMode>
 );
 
